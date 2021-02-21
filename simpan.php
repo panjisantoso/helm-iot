f1ba43707b1f092c27e4eee23a240bce3344a549
panjisantoso <panjisantoso20@gmail.com>
2021-02-21 19:22:02 +0800

    save tanggal2

--- simpan.php
+++ simpan.php
@@ -1,30 +1,31 @@
 <?php
 
 require "firebase-php-master/src/firebaseLib.php";
-$param1 = $_GET["latitude"];
-$param2 = $_GET["longitude"];
-$param3 = $_GET["bluetooth"];
-$param4 = $_GET["dateAktif"];
 
 $url = 'https://helm-iot-test-default-rtdb.firebaseio.com/'; 
 $token = 'tn8wkmGaJLQ2oT1iKeCvMeLn58BPz8EyeN1zLlsS'; 
 $DEFAULT_PATH = '/helm-iot';
 
-$tglSekarang = date("Y-m-d H:i:s");
+$from = new DateTimeZone('GMT');
+$to   = new DateTimeZone('Asia/Singapore');
+$currDate     = new DateTime('now', $from);
+$currDate->setTimezone($to);
+$currDate->format('Y-m-d H:i:s');
+
 
 if($param4 == "1" || $param4 == '1'){
-$_devicestatus= array(
-'latitude' => $param1,
-'longitude' => $param2,
-'bluetooth' => $param3,
-'tgl_helm_aktif' => $tglSekarang,
-);
+    $_devicestatus= array(
+        'latitude' => $param1,
+        'longitude' => $param2,
+        'bluetooth' => $param3,
+        'tgl_helm_aktif' => $currDate->format('Y-m-d H:i:s'),
+    );
 }else{
-$_devicestatus= array(
-'latitude' => $param1,
-'longitude' => $param2,
-'bluetooth' => $param3,
-);
+    $_devicestatus= array(
+        'latitude' => $param1,
+        'longitude' => $param2,
+        'bluetooth' => $param3,
+    );
 }
 
 
