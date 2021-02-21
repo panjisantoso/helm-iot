cbac489a05b23c7e58aa9dc9047222cd7e1a114a
panjisantoso <panjisantoso20@gmail.com>
2021-02-21 19:22:37 +0800

    save tanggal3

--- simpan.php
+++ simpan.php
@@ -2,6 +2,11 @@
 
 require "firebase-php-master/src/firebaseLib.php";
 
+$param1 = $_GET["latitude"];
+$param2 = $_GET["longitude"];
+$param3 = $_GET["bluetooth"];
+$param4 = $_GET["dateAktif"];
+
 $url = 'https://helm-iot-test-default-rtdb.firebaseio.com/'; 
 $token = 'tn8wkmGaJLQ2oT1iKeCvMeLn58BPz8EyeN1zLlsS'; 
 $DEFAULT_PATH = '/helm-iot';
