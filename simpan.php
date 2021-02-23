7c95cd3c6ddc356265fd36d9e2412a092f884bcb
panjisantoso <panjisantoso20@gmail.com>
2021-02-23 17:17:45 +0800

    bug lagi

--- simpan.php
+++ simpan.php
@@ -10,22 +10,28 @@ $url = 'https://helm-iot-test-default-rtdb.firebaseio.com/';
 $token = 'tn8wkmGaJLQ2oT1iKeCvMeLn58BPz8EyeN1zLlsS'; 
 $DEFAULT_PATH = '/helm-iot';
 
+    $_devicestatus= array(
+        'last_update' => $param4,
+        'latitude' => $param1,
+        'longitude' => $param2,
+        'bluetooth' => $param3,
+    );
+
+$firebase = new \Firebase\FirebaseLib($url, $token);
+$firebase->update($DEFAULT_PATH, $_devicestatus);
+
 $from = new DateTimeZone('GMT');
 $to   = new DateTimeZone('Asia/Singapore');
 $currDate = new DateTime('now', $from);
 $currDate->setTimezone($to);
 // $currDate->format('Y-m-d H:i:s');
-$param4 = $currDate->format('H:i:s');
+$param4 = $currDate->format('Y-m-d H:i:s');
 
-    $_devicestatus= array(
-        'latitude' => $param1,
-        'longitude' => $param2,
-        'bluetooth' => $param3,
-        'last_update' => $param4,
-    );
-
+$_devicestatus2= array(
+    'last_update' => $param4,
+);
 $firebase = new \Firebase\FirebaseLib($url, $token);
-$firebase->update($DEFAULT_PATH, $_devicestatus);
+$firebase->update($DEFAULT_PATH, $_devicestatus2);
 
 print("Update Berhasil");
 ?>
