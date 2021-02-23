3276125f9a3218c3cfdcd5a92c9720f9ebe33100
panjisantoso <panjisantoso20@gmail.com>
2021-02-24 04:10:29 +0800

    bug tgl

---
+++ remove_old_tanggal.php
@@ -0,0 +1,19 @@
+<?php
+
+
+require __DIR__.'/vendor/autoload.php';
+
+use Kreait\Firebase\ServiceAccount;
+use Kreait\Firebase\Factory;
+// $url = 'https://helm-iot-test-default-rtdb.firebaseio.com/'; 
+// $token = 'tn8wkmGaJLQ2oT1iKeCvMeLn58BPz8EyeN1zLlsS'; 
+
+$factory = (new Factory)
+        ->withServiceAccount('helm-iot-test-firebase-adminsdk-l7ipo-f4ef421923.json')
+        ->withDatabaseUri('https://helm-iot-test-default-rtdb.firebaseio.com');
+
+
+$database = $factory->createDatabase();
+$reference = $database->getReference('helm-iot/last_update');
+$reference->remove();
+?>
--- simpan.php
+++ simpan.php
@@ -14,16 +14,7 @@ $url = 'https://helm-iot-test-default-rtdb.firebaseio.com/';
 $token = 'tn8wkmGaJLQ2oT1iKeCvMeLn58BPz8EyeN1zLlsS'; 
 $DEFAULT_PATH = '/helm-iot';
 
-$factory = (new Factory)
-        ->withServiceAccount('helm-iot-test-firebase-adminsdk-l7ipo-f4ef421923.json')
-        ->withDatabaseUri('https://helm-iot-test-default-rtdb.firebaseio.com');
-        
-$database = $factory->createDatabase();
-$reference = $database->getReference('helm-iot/last_update');
-
-$snapshot = $reference->getSnapshot();
-$value = $snapshot->getChild('last_update')->getValue();
-$reference->remove();
+require_once "remove_old_tanggal.php";
 
 $from = new DateTimeZone('GMT');
 $to   = new DateTimeZone('Asia/Singapore');
@@ -32,12 +23,12 @@ $currDate->setTimezone($to);
 $currDate->format('Y-m-d H:i:s');
 $tgl = $currDate->format('Y-m-d H:i:s');
 
-    $_devicestatus= array(
-        'last_update' => $tgl,
-        'latitude' => $param1,
-        'longitude' => $param2,
-        'bluetooth' => $param3,
-    );
+$_devicestatus= array(
+    'last_update' => $tgl,
+    'latitude' => $param1,
+    'longitude' => $param2,
+    'bluetooth' => $param3,
+);
 
 $firebase = new \Firebase\FirebaseLib($url, $token);
 $firebase->update($DEFAULT_PATH, $_devicestatus);
