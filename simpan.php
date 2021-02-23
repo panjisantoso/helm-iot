6afdc867fcbd715feb46b8e6db1f9506d4f4931f
panjisantoso <panjisantoso20@gmail.com>
2021-02-23 17:25:20 +0800

    bug lagi

--- simpan.php
+++ simpan.php
@@ -5,34 +5,29 @@ require "firebase-php-master/src/firebaseLib.php";
 $param1 = $_GET["latitude"];
 $param2 = $_GET["longitude"];
 $param3 = $_GET["bluetooth"];
-$param4 = $_GET["tglUpdate"];
 
 $url = 'https://helm-iot-test-default-rtdb.firebaseio.com/'; 
 $token = 'tn8wkmGaJLQ2oT1iKeCvMeLn58BPz8EyeN1zLlsS'; 
 $DEFAULT_PATH = '/helm-iot';
-if($param4=="1" || $param4=='1'){
-    $from = new DateTimeZone('GMT');
-    $to   = new DateTimeZone('Asia/Singapore');
-    $currDate = new DateTime('now', $from);
-    $currDate->setTimezone($to);
-    // $currDate->format('Y-m-d H:i:s');
-    $tgl = $currDate->format('Y-m-d H:i:s');
+$from = new DateTimeZone('GMT');
+$to   = new DateTimeZone('Asia/Singapore');
+$currDate = new DateTime('now', $from);
+$currDate->setTimezone($to);
+// $currDate->format('Y-m-d H:i:s');
+$tgl = $currDate->format('Y-m-d H:i:s');
+
     $_devicestatus= array(
         'last_update' => $tgl,
-        'latitude' => $param1,
-        'longitude' => $param2,
-        'bluetooth' => $param3,
     );
-}else{
+
+$firebase = new \Firebase\FirebaseLib($url, $token);
+$firebase->update($DEFAULT_PATH, $_devicestatus);
+
     $_devicestatus= array(
         'latitude' => $param1,
         'longitude' => $param2,
         'bluetooth' => $param3,
     );
-}
-
-
-    
 
 $firebase = new \Firebase\FirebaseLib($url, $token);
 $firebase->update($DEFAULT_PATH, $_devicestatus);
