4ec7ed8a52b18f9366bd39311a1fae4577c8cd2d
panjisantoso <panjisantoso20@gmail.com>
2021-02-24 04:41:07 +0800

    bug tgl

--- simpan.php
+++ simpan.php
@@ -14,21 +14,16 @@ $url = 'https://helm-iot-test-default-rtdb.firebaseio.com/';
 $token = 'tn8wkmGaJLQ2oT1iKeCvMeLn58BPz8EyeN1zLlsS'; 
 $DEFAULT_PATH = '/helm-iot';
 
-// require_once "remove_old_tanggal.php";
+require_once "remove_old_tanggal.php";
 
-// $from = new DateTimeZone('GMT');
-// $to   = new DateTimeZone('Asia/Singapore');
-// $currDate = new DateTime('now', $from);
-// $currDate->setTimezone($to);
-// $tgl = $currDate->format('Y-m-d H:i:s');
-
-$date   = new DateTime(); //this returns the current date time
-$result = $date->format('Y-m-d-H-i-s');
-$krr    = explode('-', $result);
-$result = implode("", $krr);
+$from = new DateTimeZone('GMT');
+$to   = new DateTimeZone('Asia/Singapore');
+$currDate = new DateTime('now', $from);
+$currDate->setTimezone($to);
+$tgl = $currDate->format('Y-m-d H:i:s');
 
 $_devicestatus= array(
-    'last_update' => $result,
+    'last_update' => $currDate->format('Y-m-d H:i:s'),
     'latitude' => $param1,
     'longitude' => $param2,
     'bluetooth' => $param3,
