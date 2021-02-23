4d409852def9b69cdc430aa63f080179b1562461
panjisantoso <panjisantoso20@gmail.com>
2021-02-24 03:24:07 +0800

    bug tgl

--- simpan.php
+++ simpan.php
@@ -9,27 +9,28 @@ $param3 = $_GET["bluetooth"];
 $url = 'https://helm-iot-test-default-rtdb.firebaseio.com/'; 
 $token = 'tn8wkmGaJLQ2oT1iKeCvMeLn58BPz8EyeN1zLlsS'; 
 $DEFAULT_PATH = '/helm-iot';
+$firebase = new \Firebase\FirebaseLib($url, $token);
+
 $from = new DateTimeZone('GMT');
 $to   = new DateTimeZone('Asia/Singapore');
 $currDate = new DateTime('now', $from);
 $currDate->setTimezone($to);
-// $currDate->format('Y-m-d H:i:s');
+$currDate->format('Y-m-d H:i:s');
 $tgl = $currDate->format('Y-m-d H:i:s');
+
+$_devicedelete= array(
+    'last_update',
+);
+$firebase->delete($DEFAULT_PATH, $_devicedelete);
 
     $_devicestatus= array(
         'last_update' => $tgl,
-    );
-
-$firebase = new \Firebase\FirebaseLib($url, $token);
-$firebase->update($DEFAULT_PATH, $_devicestatus);
-
-    $_devicestatus= array(
         'latitude' => $param1,
         'longitude' => $param2,
         'bluetooth' => $param3,
     );
 
-$firebase = new \Firebase\FirebaseLib($url, $token);
+
 $firebase->update($DEFAULT_PATH, $_devicestatus);
 
 print("Update Berhasil");
