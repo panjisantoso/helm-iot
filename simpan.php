f8b38da987198e2bbb174925acd126204beb8204
panjisantoso <panjisantoso20@gmail.com>
2021-02-25 16:52:45 +0800

    bug tgl

--- simpan.php
+++ simpan.php
@@ -1,3 +1,5 @@
+<script src="https://www.gstatic.com/firebasejs/8.2.9/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.2.9/firebase-firestore.js"></script>
 <?php
 
 require "firebase-php-master/src/firebaseLib.php";
@@ -13,18 +15,23 @@ $param3 = $_GET["bluetooth"];
 $url = 'https://helm-iot-test-default-rtdb.firebaseio.com/'; 
 $token = 'tn8wkmGaJLQ2oT1iKeCvMeLn58BPz8EyeN1zLlsS'; 
 $DEFAULT_PATH = '/helm-iot';
+// use Google\Cloud\Core\Timestamp;
 
-require_once "remove_old_tanggal.php";
+// ....
+// $from = new DateTimeZone('GMT');
+// $to   = new DateTimeZone('Asia/Singapore');
+// $stamp = new Timestamp(new DateTime());
+// echo $stamp;
+// require_once "remove_old_tanggal.php";
 
 $from = new DateTimeZone('GMT');
 $to   = new DateTimeZone('Asia/Singapore');
 $currDate = new DateTime('now', $from);
 $currDate->setTimezone($to);
-$tgl1 = $currDate->format('Y-m-d');
-$tgl2 = $currDate->format('H:i:s');
-$result = $tgl1 . ' ' . $tgl2;
+$tgl = $currDate->format('Y-m-d H:i:s');
+
 $_devicestatus= array(
-    'last_update' => $result,
+    'last_update' => $tgl,
     'latitude' => $param1,
     'longitude' => $param2,
     'bluetooth' => $param3,
