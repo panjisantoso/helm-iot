ced6e0568ad0a6695d152437b4fdef37b832cce1
panjisantoso <panjisantoso20@gmail.com>
2021-06-23 19:36:53 +0800

    id arduino

--- simpan.php
+++ simpan.php
@@ -9,16 +9,13 @@ use Kreait\Firebase\Factory;
 $param1 = $_GET["latitude"];
 $param2 = $_GET["longitude"];
 $param3 = $_GET["bluetooth"];
+$param4 = $_GET["arduinoId"];
 
 $url = 'https://helm-iot-test-default-rtdb.firebaseio.com/'; 
 $token = 'tn8wkmGaJLQ2oT1iKeCvMeLn58BPz8EyeN1zLlsS'; 
-$DEFAULT_PATH = '/helm-iot';
 
-// $from = new DateTimeZone('GMT');
-// $to   = new DateTimeZone('Asia/Singapore');
-// $currDate = new DateTime('now', $from);
-// $currDate->setTimezone($to);
-// $tgl = $currDate->format('Y-m-d H:i:s');
+$DEFAULT_PATH = '/helm-iot/$arduinoId'; /*sesuaiin setelah "/helm-iot/" dengan address bluetooth slave*/
+
 date_default_timezone_set("Asia/Singapore");
 $tgl = date("Y-m-d H:i:s");
 
@@ -27,6 +24,8 @@ $_devicestatus= array(
     'latitude' => $param1,
     'longitude' => $param2,
     'bluetooth' => $param3,
+    'helm_name' => $param4,
+    'alarm_helm' => "0",
 );
 
 $firebase = new \Firebase\FirebaseLib($url, $token);
